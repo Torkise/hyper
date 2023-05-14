1325696e3aa3a2678a0b88b509f8019a904b8f9a
Andreas <andreas.bo@hotmail.no>
2023-05-14 17:40:48 +0200

    fixes

--- Frontend/src/views/AreasView.vue
+++ Frontend/src/views/AreasView.vue
@@ -9,28 +9,30 @@
         <li><a href="#healthcare"><img src="../assets/icons/healthcare.svg"><span>Healthcare</span></a></li>
       </ul>
     </nav>
-
     <h1 class="page-title">Our Fields of Interest</h1>
-    <p>In Genesis Capital, we are committed to investing in innovative and high-growth potential startups that are...</p>
-
-    <section id="general">
+    <section id="general" class="area-section">
       <!-- Content for Section 1 -->
+      <p>In Genesis Capital, we are committed to investing in innovative and high-growth potential startups that are poised to make a significant impact in their respective industries. On this page, we provide an overview of the areas in which we invest and the types of businesses we are looking to support. Whether you are an entrepreneur seeking funding for your startup or an investor interested in learning more about our investment strategy, we hope this page provides valuable insights into our focus and approach.</p>
     </section>
 
-    <section id="energy">
+    <section id="energy" class="area-section">
       <!-- Content for Section 2 -->
+      <h2 class="page-title">Energy</h2>
     </section>
 
-    <section id="sustainability">
+    <section id="sustainability" class="area-section">
       <!-- Content for Section 3 -->
+      <h2 class="page-title">Sustainability</h2>
     </section>
 
-    <section id="technology">
+    <section id="technology" class="area-section">
       <!-- Content for Section 4 -->
+      <h2 class="page-title">Technology</h2>
     </section>
 
-    <section id="healthcare">
+    <section id="healthcare" class="area-section">
       <!-- Content for Section 5 -->
+      <h2 class="page-title">Healthcare</h2>
     </section>
   </div>
 </template>
@@ -42,6 +44,10 @@
     flex-direction: column;
     padding: 8rem;
     margin-left: 6rem;
+  }
+
+  .area-section {
+    margin-bottom: 4rem;
   }
 
   .menu {
